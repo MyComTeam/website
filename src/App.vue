12924d4aac326b21d094cfcb8ca78f98c0c092ca
AdikWeb <adilhan97kz@gmail.com>
2019-09-08 17:49:09 +0600

    common setting

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,22 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+	<div id="app">
+		<header-main></header-main>
+		<router-view/>
+	</div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+// eslint-disable-next-line
+import commonstyle from './styles/commonstyle.sass' 
+import headerMain from './components/layout/header-main'
+
+export default {
+	components: {
+		headerMain
+	}
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+</script>
+
+<style lang="sass" commonstyle>
+
 </style>
---
+++ src/components/layout/header-main.vue
@@ -0,0 +1,39 @@
+<template>
+	<header>
+		<div class="header__top-panel">
+			<div class="header__top-panel--navbar">
+				<nav>
+					<router-link to="/">Home</router-link>
+					<router-link to="/about">About</router-link>
+				</nav>
+			</div>
+			<div class="header__top-panel--logotype-wrapper">
+				<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/512px-Vue.js_Logo_2.svg.png" alt="">
+			</div>
+		</div>
+	</header>
+</template>
+
+<script>
+export default {
+	name: 'HeaderMain',
+	components: {
+	}
+}
+</script>
+
+<style lang="sass" commonstyle>
+	.header
+		&__top-panel
+			display: flex
+			align-items: center
+			&--navbar
+				nav
+					a
+						display: inline-block
+						text-decoration: none
+			&--logotype-wrapper
+				img
+					width: 50px
+					display: block
+</style>
